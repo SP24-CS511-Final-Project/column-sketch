25d39cac866bf257527acd4abdbb1242e11fde74
Xixiang-Liu <xixiangliuoscar@gmail.com>
2024-04-01 18:22:12 -0500

    seperate functions get_sortedness and get_sortedness_block

--- parquet.py
+++ parquet.py
@@ -9,21 +9,21 @@ def print_pq(table_name: str) -> None:
     df = table.to_pandas()
     print(df)
 
-def get_sortedness(df: pd.DataFrame, size_block=512) -> float:
-    def get_sortedness_block(block: pd.DataFrame) -> float:
-        N = len(block)
-        asc = desc = eq = 0
-        for i in range(N - 1):
-            if block.iloc[i]["col"] < block.iloc[i + 1]["col"]:
-                asc += 1
-            elif block.iloc[i]["col"] == block.iloc[i + 1]["col"]:
-                eq += 1
-            else:
-                desc += 1
-        sortedness = ((max(asc, desc) + eq - math.floor(N / 2))
-                      / (math.ceil(N / 2) - 1))
-        return sortedness
+def get_sortedness_block(block: pd.DataFrame) -> float:
+    N = len(block)
+    asc = desc = eq = 0
+    for i in range(N - 1):
+        if block.iloc[i]["col"] < block.iloc[i + 1]["col"]:
+            asc += 1
+        elif block.iloc[i]["col"] == block.iloc[i + 1]["col"]:
+            eq += 1
+        else:
+            desc += 1
+    sortedness = ((max(asc, desc) + eq - math.floor(N / 2))
+                  / (math.ceil(N / 2) - 1))
+    return sortedness
 
+def get_sortedness(df: pd.DataFrame, size_block=512) -> float:
     num_rows = len(df)
     num_full_blocks = num_rows // size_block
 
@@ -47,5 +47,3 @@ def get_sortedness(df: pd.DataFrame, size_block=512) -> float:
 df = pd.DataFrame({"col": [random.uniform(0, 100) for _ in range(1000)]})
 table = pa.Table.from_pandas(df)
 pq.write_table(table, "example.parquet")
-
-
