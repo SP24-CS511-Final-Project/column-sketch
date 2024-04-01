1881a6bb93666f19622c51a6169123712f2cd03f
Xixiang-Liu <xixiangliuoscar@gmail.com>
2024-04-01 16:26:41 -0500

    implement function print_pq

--- parquet.py
+++ parquet.py
@@ -3,6 +3,13 @@ import pyarrow as pa
 import random
 import pyarrow.parquet as pq
 
+def print_pq(table_name: str) -> None:
+    table = pq.read_table(table_name)
+    df = table.to_pandas()
+    print(df)
+
 df = pd.DataFrame({"col": [random.uniform(0, 100) for _ in range(1000)]})
 table = pa.Table.from_pandas(df)
 pq.write_table(table, "example.parquet")
+
+
